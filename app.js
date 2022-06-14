86300f1c34ed94bdf2c27021bc6d0df2a64b31a1
ksyoung <pionir76@gmail.com>
2022-06-14 16:02:00 +0900

    Second

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const canvas = document.getElementById("jsCanvas");
 
 const ctx = canvas.getContext("2d");
@@ -15,3 +17,37 @@ function onMouseMove(event) {
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
 }
+
+const func = () => console.log("asdfasdf");
+
+class Counter {
+  constructor(callBack) {
+    this.counter = 0;
+    this.callBack = callBack;
+  }
+
+  increase(callBackFunc) {
+    this.counter++;
+    console.log(this.counter);
+
+    if (this.counter % 5 === 0) {
+      this.callBack();
+    }
+  }
+}
+
+const cnt = new Counter(printSomething);
+
+function printSomething() {
+  console.log("test....");
+}
+
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
+cnt.increase();
---
+++ readMe.md
@@ -0,0 +1,11 @@
+# My First... Heading 1
+
+## Heading 2...
+
+---
+
+This is the **bold**
+
+- chekc.
+- chekc. 2
+  > what is this.
